ca1238ae8a1a5b61b1500c426d5c794dc659785a
unfv-unajma <141893949+unfv-unajma@users.noreply.github.com>
2023-08-09 23:00:50 -0500

    Add files via upload

---
+++ assets/js/functions.js
@@ -0,0 +1,9 @@
+const hideScrollFP = () => {
+  console.log("no funciona");
+  fullpage_api.setAllowScrolling(false);
+}
+
+const showScrollFP = () => {
+  console.log("funciona");
+  fullpage_api.setAllowScrolling(true);
+}
